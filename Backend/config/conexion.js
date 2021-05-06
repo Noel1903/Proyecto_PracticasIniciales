9a81fc162af0d858acce3bd192d5c9b3b84610b4
IsaForkel <56815499+IsaForkel@users.noreply.github.com>
2021-05-06 04:43:45 -0600

    usuario normal fi

--- Backend/config/conexion.js
+++ Backend/config/conexion.js
@@ -2,13 +2,8 @@ const mysql=require('mysql');
 const conexion=mysql.createConnection({
     host: 'localhost',
     user:'root',
-<<<<<<< HEAD
-    password:'Noel19Ch@con',//Aqui se escribe su nombre de usuario de SQL
-    port:3306,
-=======
-    password:'Noel19Ch@con',//Aqui se escribe su nombre de usuario de SQL
-    port:3306,
->>>>>>> 697935714092f7d8f80fccc477f9d9b8f1637c0c
+    password:'wertyuk',//Aqui se escribe su nombre de usuario de SQL
+    port:3307,
     database: 'db_datos001'
 
 });
